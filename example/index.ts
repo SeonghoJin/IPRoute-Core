ab6954624ecea4b4719a478f184785a7e8f73141
moretall <seongho4996@naver.com>
2021-11-12 19:06:43 +0900

    add example

--- example/index.ts
+++ example/index.ts
@@ -1,1 +1,12 @@
-import {} from 'ip-trace';
+import {Hop, iptrace} from 'ip-trace'
+
+const tracer = iptrace('naver.com');
+
+const maxHop = 5;
+
+tracer.onHop((hop: Hop) => {
+    if(hop.hop > maxHop){
+        tracer.end();
+    }
+    console.log(hop);
+}).start();
